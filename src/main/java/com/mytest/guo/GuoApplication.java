719cfb66084c37e807def6e9c1809959650fba43
guojunchao <859373875@qq.com>
2020-04-20 21:07:07 +0800

    初始化

---
+++ src/main/java/com/mytest/guo/GuoApplication.java
@@ -0,0 +1,13 @@
+package com.mytest.guo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuoApplication.class, args);
+	}
+
+}
